4742a446d423ec972e11e5cabedf5cb81b526113
Preston Booth <preston.t.booth@gmail.com>
2021-10-12 10:09:58 -0600

    Refactored Folder & Filenames

---
+++ codejournal.js
@@ -0,0 +1,21 @@
+var name; //global scope 
+
+let foo; // declared variable that can be changed
+
+const ANSWER = 42; //const is declared and initialized with the value 42
+
+let string1 = "Hello World"; //this is a 'string literal'
+let string2 = new String("HELLO WORLD!")  //uses a constructor
+
+
+// Number
+let myNum = 20389743;
+
+let myDecimal = 73.4;
+
+
+// Boolean
+
+let myBool = true;
+
+print(string1);
