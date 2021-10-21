76a35052d2fc61238d42b38dd852e5dcc39613ae
Preston Booth <preston.t.booth@gmail.com>
2021-10-21 10:44:42 -0600

    Got Character Images To Display

--- Characters/sw-chars.js
+++ Characters/sw-chars.js
@@ -3,11 +3,16 @@ import { people } from '../data/people.js'
 const mainContent = document.querySelector('#main')
 
 
-people.forEach(element => {
+people.forEach((element, index) => {
     const charFigure = document.createElement('figure')
     const charImg = document.createElement('img')
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
+
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${index+1}.jpg`
+    const charCaption = document.createElement('figcaption')
+    charCaption.textContent = element.name;
+
     charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
     mainContent.appendChild(charFigure)
 
 
