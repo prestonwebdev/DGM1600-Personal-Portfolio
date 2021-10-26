30377104c832c2a1172d4e74b8c99f880395dea5
Preston Booth <preston.t.booth@gmail.com>
2021-10-26 10:30:24 -0600

    Added get last number function

--- Characters/sw-chars.js
+++ Characters/sw-chars.js
@@ -3,19 +3,33 @@ import { people } from '../data/people.js'
 const mainContent = document.querySelector('#main')
 
 
-people.forEach((element, index) => {
+people.forEach((element) => {
+
     const charFigure = document.createElement('figure')
     const charImg = document.createElement('img')
-
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${index+1}.jpg`
+    const charNum = getLastNumber(element.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+  
     const charCaption = document.createElement('figcaption')
-    charCaption.textContent = element.name;
-
+    charCaption.textContent = element.name
     charFigure.appendChild(charImg)
     charFigure.appendChild(charCaption)
     mainContent.appendChild(charFigure)
 
 
+
 })
 
+function getLastNumber(url)
+{
+  let end = url.lastIndexOf('/')
+  let start = end - 2
+  if(url.charAt(start) === '/'){
+      start++;
+  }
+  
+  return url.slice(start, end)
+}
 
+
+getLastNumber('https//:alskdfjasdf /.com')
