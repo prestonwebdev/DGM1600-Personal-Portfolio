c1be00866c3beeb4bccb3bd0f698fb5c2c0d34c2
Preston Booth <preston.t.booth@gmail.com>
2021-10-28 10:35:38 -0600

    film titles now match!

--- Characters/sw-chars.js
+++ Characters/sw-chars.js
@@ -1,4 +1,5 @@
 import { people } from '../data/people.js'
+import { getLastNumber } from '../utils/index.js'
 
 const mainContent = document.querySelector('#main')
 
@@ -17,7 +18,7 @@ const header = document.createElement('header')
 const maleButton = document.createElement('button')
 maleButton.textContent = 'Male Characters'
 
-populateDOM(otherCharacters)
+populateDOM(people)
 
 maleButton.addEventListener('click', () => populateDOM(maleCharacters))
 
@@ -51,11 +52,3 @@ function populateDOM(characters) {
   })
 }
 
-function getLastNumber(url) {
-  let end = url.lastIndexOf('/')
-  let start = end - 2
-  if (url.charAt(start) === '/') {
-    start++
-  }
-  return url.slice(start, end)
-}
--- films/sw-films.js
+++ films/sw-films.js
@@ -1,4 +1,6 @@
 import { films } from '../data/films.js'
+import { getLastNumber } from '../utils/index.js'
+
 
 let filmList = document.querySelector('#filmList')
 
@@ -8,7 +10,14 @@ let figure = document.createElement('figure')
 let figImg = document.createElement('img')
 figImg.src = `https://starwars-visualguide.com/assets/img/films/${i+1}.jpg`
 let figCaption = document.createElement('figcaption')
-figCaption.textContent = films[i];
+
+
+const foundFilm = films.find(film => {
+    const convertedString = parseInt(getLastNumber(film.url), 10)
+    return convertedString === (i+1)
+})
+
+figCaption.textContent = foundFilm.title;
 
 figure.appendChild(figImg)
 figure.appendChild(figCaption)
