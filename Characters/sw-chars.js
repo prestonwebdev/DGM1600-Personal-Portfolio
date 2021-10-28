645dbb70312b102a70c0dc93f5f43e1df6015c30
Preston Booth <preston.t.booth@gmail.com>
2021-10-28 09:52:14 -0600

    Fixed Errors From Prev. Class

--- Characters/sw-chars.js
+++ Characters/sw-chars.js
@@ -1,76 +1,61 @@
-import { people } from "../data/people.js";
+import { people } from '../data/people.js'
 
-const mainContent = document.querySelector("#main");
+const mainContent = document.querySelector('#main')
 
-const maleCharacters = people.filter((person) => person.gender === 'male');
-const femaleCharacters = people.filter((person) => person.gender === 'female');
-const otherCharacters = peoples.filter((person => {
-    if(person.gender === 'hermaphrodite' || person.gender === 'n/a' || person.gender === 'none'){
-        return person
-    }
-}))
+const maleCharacters = people.filter((person) => person.gender === 'male')
+const femaleCharacters = people.filter((person) => person.gender === 'female')
+const otherCharacters = people.filter((person) => {
+  if (
+    person.gender !== 'male' &&
+    person.gender !== 'female' 
+  ) {
+    return person
+  }
+})
 
+const header = document.createElement('header')
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
 
-const header = document.createElement('header');
-const maleButton = document.createElement('button');
-maleButton.textContent = "Male Characters";
+populateDOM(otherCharacters)
 
-poplulateDOM(people)
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
 
-maleButton.addEventListener('click', () => poplulateDOM(maleCharacters))
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
 
-const femaleButton = document.createElement('button');
-femaleButton.textContent = 'Female Characters';
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
 
-femaleButton.addEventListener('click', () => poplulateDOM(femaleCharacters))
+header.appendChild(maleButton)
+header.appendChild(femaleButton)
 
-header.appendChild(maleButton);
-header.appendChild(femaleButton);
-header.appendChild(otherbutton);
+document.body.insertBefore(header, mainContent)
 
+function populateDOM(characters) {
+  // clear the page first, then populate
+  while (mainContent.firstChild) {
+    mainContent.removeChild(mainContent.firstChild)
+  }
+  characters.forEach((element) => {
+    const charFigure = document.createElement('figure')
+    const charImg = document.createElement('img')
+    const charNum = getLastNumber(element.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
 
-document.body.insertBefore(header, mainContent);
+    const charCaption = document.createElement('figcaption')
+    charCaption.textContent = element.name
 
-const maleCharacters = people.filter((person) => person.gender === 'male');
-const femaleCharacters = people.filter((person) => person.gender === 'female');
-const otherCharacters = peoples.filter((person => {
-    if(person.gender === 'hermaphrodite' || person.gender === 'n/a' || person.gender === 'none'){
-        retrun person;
-    }
-}))
-
-function poplulateDOM(characters) {
-    //clear the page first, then populate
-
-    while (mainContent.firstChild){
-        mainContent.removeChild(mainContent.firstChild);
-    }
-
-
-
-    characters.forEach((element) => {
-    const charFigure = document.createElement("figure");
-    const charImg = document.createElement("img");
-    const charNum = getLastNumber(element.url);
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
-
-    const charCaption = document.createElement("figcaption");
-    charCaption.textContent = element.name;
-
-    charFigure.appendChild(charImg);
-    charFigure.appendChild(charCaption);
-    mainContent.appendChild(charFigure);
-  });
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+    mainContent.appendChild(charFigure)
+  })
 }
 
 function getLastNumber(url) {
-  let end = url.lastIndexOf("/");
-  let start = end - 2;
-  if (url.charAt(start) === "/") {
-    start++;
+  let end = url.lastIndexOf('/')
+  let start = end - 2
+  if (url.charAt(start) === '/') {
+    start++
   }
-
-  return url.slice(start, end);
+  return url.slice(start, end)
 }
-
-getLastNumber("https//:alskdfjasdf /.com");
