616bc8a30e25e1cef87e60b121a54999e7d92d7f
Preston Booth <preston.t.booth@gmail.com>
2021-11-05 09:13:38 -0600

    Added Cards & Extra Button

--- Characters/sw-chars.js
+++ Characters/sw-chars.js
@@ -15,22 +15,119 @@ const otherCharacters = people.filter((person) => {
 })
 
 const header = document.createElement('header')
+const hero = document.createElement('section')
+hero.className = "media-hero"
+
+const heroBody = document.createElement('div')
+heroBody.className = "media-hero-body"
+
+
+
+//Create Filter Buttons
 const maleButton = document.createElement('button')
 maleButton.textContent = 'Male Characters'
-//populateDOM(people)
 
-maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+const otherButton = document.createElement('button')
+otherButton.textContent = 'Other Characters'
 
 const femaleButton = document.createElement('button')
 femaleButton.textContent = 'Female Characters'
 
+
+populateDOM(people)
+
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+
 femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
 
-header.appendChild(maleButton)
-header.appendChild(femaleButton)
+otherButton.addEventListener('click', () => populateDOM(otherCharacters))
+
+
+
+const filters = document.createElement('div')
+filters.className ="filterDiv"
+mainContent.appendChild(filters)
+filters.appendChild(maleButton)
+filters.appendChild(femaleButton)
+filters.appendChild(otherButton)
 
 document.body.insertBefore(header, mainContent)
+document.body.insertBefore(filters, mainContent)
 
+
+
+
+
+function populateDOM(characters) {
+  // clear the page first, then populate
+  removeChildren(mainContent)
+  characters.forEach((element) => {
+
+    //create a bulma "card div"
+    const cardDiv = document.createElement('div')
+    cardDiv.className = "card"
+
+    //create a bulma "image div"
+    const imageDiv = document.createElement('div')
+    imageDiv.className = "card-image is-centered"
+
+
+    //create a bulma "image div"
+     const contentDiv = document.createElement('div')
+     contentDiv.className = "card-content"
+
+     //create bulma "media div"
+     const media = document.createElement('div')
+     media.className = "media-content"
+
+     
+     const charName = document.createElement('p')
+     charName.className = "title is-4"
+     charName.textContent = element.name
+
+     const birthDate = document.createElement('p')
+     birthDate.className = "subtitle is-6"
+     birthDate.textContent = "Birth Year: " + element.birth_year
+
+
+     const charGender = document.createElement('p')
+     charGender.className = "subtitle is-6"
+     charGender.textContent = "Gender: " + element.gender
+
+
+     
+
+
+
+
+    const charFigure = document.createElement('figure')
+    charFigure.className = "image is-4by3 is-centered"
+
+
+    const charImg = document.createElement('img')
+    const charNum = getLastNumber(element.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+
+    const charCaption = document.createElement('figcaption')
+    charCaption.textContent = element.name
+
+    
+    cardDiv.appendChild(imageDiv)
+    cardDiv.appendChild(contentDiv)
+    
+    imageDiv.appendChild(charFigure)
+    charFigure.appendChild(charImg)
+
+    
+    contentDiv.appendChild(media)
+    media.appendChild(charName)
+    media.appendChild(birthDate)
+    media.appendChild(charGender)
+    mainContent.appendChild(cardDiv)
+  })
+}
+
+/*
 function populateDOM(characters) {
   // clear the page first, then populate
   removeChildren(mainContent)
@@ -48,3 +145,4 @@ function populateDOM(characters) {
     mainContent.appendChild(charFigure)
   })
 }
+*/
