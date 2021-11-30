4d67dbe8136228a0fee8aebec986a78586accf5b
prestonwebdev <preston.t.booth@gmail.com>
2021-11-30 11:09:18 -0700

    Class11.31.21

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,11 +1,48 @@
 async function getAPIData(url) {
     try {
-        const response = await fetch(url)
-        const data = await response.json()
-
-    } catch (error){
-
-        console.error(error)
+      const response = await fetch(url)
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.error(error)
     }
-}
+  }
+  
+  getAPIData(`https://pokeapi.co/api/v2/pokemon/25`)
+  .then((data) => {
+    console.log(data)
+    populatePokeCards(data)
+  })
+  
+  const pokeGrid = document.querySelector('.pokeGrid')
+  
+  function populatePokeCards(singlePokemon) {
+    const pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    const pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener('click', () => 
+        pokeCard.classList.toggle('is-flipped')
+    )
+    const front = populateCardFront(singlePokemon)
+    const pokeBack = document.createElement('div')
+    pokeBack.className = 'cardFace back'
+    pokeBack.textContent = 'Back'
+  
+    pokeCard.appendChild(front)
+    pokeCard.appendChild(pokeBack)
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+  }
 
+  function populateCardFront(pokemon) {
+    const pokeFront = document.createElement('figure')
+    pokeFront.className = 'cardFace front'
+    const pokeImg = document.createElement('img')
+    pokeImg.src = 'https://raw.gitgubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png'
+    const pokeCaption = document.createElement('figcaption')
+    pokeCaption.textContent = 'Front'
+    pokeFront.appendChild(pokeImg)
+    pokeFront.appendChild(pokeCaption)
+    return pokeFront
+  }
