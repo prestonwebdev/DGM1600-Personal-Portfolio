76df72cedec5ee958d1a493d110ce89364393a0b
prestonwebdev <preston.t.booth@gmail.com>
2021-12-07 16:05:03 -0700

    Need Help With Find Pokemon Function

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,36 +1,23 @@
 import { removeChildren } from '../utils/index.js'
 
-function getAPIData(url) {
-  try {
-    return fetch(url).then((data) => data.json())
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-function loadPokemon(offset = 0, limit = 25) {
-  getAPIData(
-    `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`,
-  ).then(async (data) => {
-    for (const pokemon of data.results) {
-      await getAPIData(pokemon.url).then((pokeData) =>
-        populatePokeCard(pokeData),
-      )
-    }
-  })
-}
-
-const findPoke = document.querySelector('#searchBar')
-const pokeGrid = document.querySelector('.pokeGrid')
-const loadButton = document.querySelector('.loadPokemon')
+//Query Selectors
+let input = document.querySelector("#searchBar") //search
+const pokeGrid = document.querySelector('.pokeGrid')  // Pokemon Grid
+const loadButton = document.querySelector('.loadPokemon')  // Load Pokemon Button
+const newButton = document.querySelector('.newPokemon')  //Create A Pokemon Button
+const morePokemon = document.querySelector('.morePokemon') //Load More Pokemon Button
 
 
 
+//Event Listeners 
+
+//Load  Pokemon Button Event Listener
 loadButton.addEventListener('click', () => {
   removeChildren(pokeGrid)
   loadPokemon()
 })
-const newButton = document.querySelector('.newPokemon')
+
+//New Pokemon Button Event Listener
 newButton.addEventListener('click', () => {
   let pokeName = prompt('What is the name of your new Pokemon?')
   let pokeHeight = prompt('What is the height of your Pokemon?')
@@ -47,13 +34,51 @@ newButton.addEventListener('click', () => {
   populatePokeCard(newPokemon)
 })
 
-const morePokemon = document.querySelector('.morePokemon')
+
+//Load More Pokemon Event Listener
 morePokemon.addEventListener('click', () => {
   let startPoint = prompt('Which pokemon ID do we start with?')
   let howMany = prompt('How many more Pokemon do you want to see?')
   loadPokemon(startPoint, howMany)
 })
 
+
+
+//Search For Pokemon Event Listener
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    let userIn = input.value
+    findPokemon(userIn)
+  }
+});
+
+
+//Functions 
+
+//Get Pokemon Data From The API
+function getAPIData(url) {
+  try {
+    return fetch(url).then((data) => data.json())
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+//Function To Load Pokemon To the DOM
+function loadPokemon(offset = 0, limit = 25) {
+  getAPIData(
+    `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`,
+  ).then(async (data) => {
+    for (const pokemon of data.results) {
+      await getAPIData(pokemon.url).then((pokeData) =>
+        populatePokeCard(pokeData),
+      )
+    }
+  })
+}
+
+//Get Pokemon Abilities Array
 function getAbilitiesArray(commaString) {
   let tempArray = commaString.split(',')
   console.log(tempArray)
@@ -66,26 +91,31 @@ function getAbilitiesArray(commaString) {
   })
 }
 
-let input = document.querySelector("#searchBar")
 
 
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    let userIn = input.value
-    findPokemon(userIn)
-  }
-});
 
+
+//Funtion to Find Pokemon at the entered Value
 function findPokemon(value){
- 
+
+  getAPIData(
+    `https://pokeapi.co/api/v2/pokemon/${value}`,
+  ).then(async (data) => {
+    for (const pokemon of data.results) {
+      await getAPIData(data.res.url).then((pokeData) =>
+        populatePokeCard(pokeData),
+      )
+    }
+  })
+}
+
   
-  loadPokemon(value-1, 1)
-
-};
-
-findPokemon()
 
 
+
+
+
+//Function To Popoulate the Pokemon Card 
 function populatePokeCard(singlePokemon) {
   const pokeScene = document.createElement('div')
   pokeScene.className = 'scene'
@@ -103,6 +133,13 @@ function populatePokeCard(singlePokemon) {
   pokeGrid.appendChild(pokeScene)
 }
 
+
+
+
+
+
+
+//Populate the Front Of the Pokemon Card 
 function populateCardFront(pokemon) {
   const pokeFront = document.createElement('figure')
   pokeFront.className = 'cardFace front'
@@ -113,12 +150,18 @@ function populateCardFront(pokemon) {
     pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
   }
   const pokeCaption = document.createElement('figcaption')
-  pokeCaption.textContent = `${pokemon.id} ${pokemon.name}`
+  pokeCaption.textContent = `${pokemon.name}`
   pokeFront.appendChild(pokeImg)
   pokeFront.appendChild(pokeCaption)
+
+  typesBackground(pokemon, pokeFront)
+  
   return pokeFront
 }
 
+
+
+//Populate The Back Of The Pokemon Card
 function populateCardBack(pokemon) {
   const pokeBack = document.createElement('div')
   pokeBack.className = 'cardFace back'
@@ -135,6 +178,64 @@ function populateCardBack(pokemon) {
   return pokeBack
 }
 
+
+//Changes Card Color Based on Pokemon Type
+function typesBackground(pokemon, card) {
+  let pokeType1 = pokemon.types[0].type.name
+  let pokeType2 = pokemon.types[1]?.type.name
+  if (!pokeType2) {
+    card.style.setProperty('background', getPokeTypeColor(pokeType1))
+  } else {
+    card.style.setProperty('background',
+    `linear-gradient(${getPokeTypeColor(pokeType1)}, ${getPokeTypeColor(pokeType2)})`)
+  }
+}
+
+
+//Gets the type of the pokemon and returns the color for that type to be modified. 
+function getPokeTypeColor(pokeType) {
+  let color
+  switch (pokeType) {
+    case 'grass':
+      color = '#00ff00'
+      break
+      case 'fire':
+      color = '#ff0000'
+      break
+      case 'water':
+      color = '#0000ff'
+      break
+      case 'bug':
+      color = '#7fff00'
+      break
+      case 'normal':
+      color = '#f5f5dc'
+      break
+      case 'flying':
+      color = '#00ffff'
+      break
+      case 'poison':
+      color = '#c300ff'
+      break
+      case 'electric':
+      color = '#c8ff00'
+      break
+      case 'psychic':
+      color = '#e96c95'
+      break
+      case 'ground':
+      color = '#ceb250'
+      break
+      case 'rock':
+      color = '#444444'
+      break
+      default:
+        color = '#999999'
+  }
+  return color
+}
+
+// Object Class For Creating a New Pokemon 
 class Pokemon {
   constructor(name, height, weight, abilities) {
     ;(this.id = 9001),
